41891b94607a8a8c54f547eaeb898844bce8d0a4
sajithajong <sajithajong91@gmail.com>
2026-07-01 20:38:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ document.addEventListener("DOMContentLoaded", () => {
         alert("🚀 Payment system coming soon!\nTaklufx XML Store");
     });
 });
+const popup=document.getElementById("paymentPopup");
+
+document.querySelectorAll(".buy").forEach(btn=>{
+btn.onclick=()=>{
+popup.style.display="flex";
+}
+});
+
+document.querySelector(".close").onclick=()=>{
+popup.style.display="none";
+}
+
+window.onclick=(e)=>{
+if(e.target==popup){
+popup.style.display="none";
+}
+}
+
+document.getElementById("paidBtn").onclick=()=>{
+alert("Thanks! Please send your payment screenshot on WhatsApp. XML will be provided after verification.");
+}
